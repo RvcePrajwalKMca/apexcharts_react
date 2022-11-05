7371144c8860f9b5d25d8864b24cc323e9c1271a
prajwalk <prajwalk.mca21@rvce.edu.in>
2022-11-05 18:04:08 +0530

    live chart done

--- src/Livechart.jsx
+++ src/Livechart.jsx
@@ -5,56 +5,58 @@ import ReactApexChart from "react-apexcharts";
 // import mqtt from "mqtt";
 
 function Livechart() {
-  // var [count, setCount] = useState({
-  //   value: Math.floor(Math.random() * 101).toFixed(2),
-  // });
   function get_y_axis(y_val) {
     const y = y_val;
-    console.log(y);
     sety_axis([...y_axis, y]);
   }
 
   function get_x_axis(x_val) {
     let x = x_val;
-    console.log(x);
     setx_axis([...x_axis, x]);
   }
+  // var cnt = 0;
   const [x_axis, setx_axis] = useState([]);
   const [y_axis, sety_axis] = useState([]);
   const [Volt_R, setVolt_R] = useState({
     series: [
       {
         name: "Voltage-R",
-        data: [],
+        data: y_axis,
       },
     ],
     options: {
       chart: {
         height: 350,
         type: "area",
-        animations: {
-          enabled: true,
-          easing: "easeinout",
-          speed: 800,
-          animateGradually: {
-            enabled: true,
-            delay: 150,
-          },
-          dynamicAnimation: {
-            enabled: true,
-            speed: 350,
-          },
+      },
+      noData: {
+        text: "Loading...",
+        align: "center",
+        verticalAlign: "middle",
+        offsetX: 0,
+        offsetY: 0,
+        style: {
+          color: "#000000",
+          fontSize: "14px",
+          fontFamily: "Helvetica",
+        },
+      },
+      markers: {
+        size: 2,
+        hover: {
+          size: undefined,
+          sizeOffset: 3,
         },
       },
       dataLabels: {
-        enabled: true,
+        enabled: false,
       },
       stroke: {
         curve: "smooth",
       },
       xaxis: {
         type: "datetime",
-        categories: [],
+        categories: x_axis,
       },
       tooltip: {
         x: {
@@ -76,22 +78,32 @@ function Livechart() {
         chart: {
           height: 350,
           type: "area",
-          animations: {
+          zoom: {
             enabled: true,
-            easing: "easeinout",
-            speed: 800,
-            animateGradually: {
-              enabled: true,
-              delay: 150,
-            },
-            dynamicAnimation: {
-              enabled: true,
-              speed: 350,
+            type: "x",
+            autoScaleYaxis: false,
+            zoomedArea: {
+              fill: {
+                color: "#90CAF9",
+                opacity: 0.4,
+              },
+              stroke: {
+                color: "#0D47A1",
+                opacity: 0.4,
+                width: 1,
+              },
             },
           },
         },
+        markers: {
+          size: 7,
+          hover: {
+            size: undefined,
+            sizeOffset: 3,
+          },
+        },
         dataLabels: {
-          enabled: true,
+          enabled: false,
         },
         stroke: {
           curve: "smooth",
@@ -102,32 +114,24 @@ function Livechart() {
         },
         tooltip: {
           x: {
-            format: "HH:mm:ss",
+            format: "dd MMM yyyy HH:mm:ss",
           },
         },
       },
     };
   }
 
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     sety_axis((y) => get_y_axis(y_axis));
-  //   }, 1000);
-  //   return () => clearInterval(interval);
-  // }, [y_axis]);
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setx_axis((x) => get_x_axis(x_axis));
-  //   }, 1000);
-  //   return () => clearInterval(interval);
-  // }, [x_axis]);
-
   useEffect(() => {
     const interval = setInterval(() => {
       get_x_axis(new Date().toISOString());
       get_y_axis(Math.floor(Math.random() * 101).toFixed(2));
+
+      // cnt = cnt + 1;
+      // console.log(cnt);
+
       setVolt_R((x) => getData1());
+      // console.log("x_array length : " + x_axis.length);
+      // console.log("y_array length : " + y_axis.length);
     }, 1000);
     return () => clearInterval(interval);
   }, [Volt_R, x_axis, y_axis]);
@@ -141,7 +145,7 @@ function Livechart() {
         series={Volt_R.series}
         type="line"
         height={350}
-        width={500}
+        width={400}
       />
       {/* <h2>{count.value}</h2> */}
     </div>
