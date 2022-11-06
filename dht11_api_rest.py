e43f9cb51c1516ea50854314fca5e389c7f86c4d
prajwalk <prajwalk.mca21@rvce.edu.in>
2022-11-07 02:16:08 +0530

    dht11_rest_api added

---
+++ dht11_api_rest.py
@@ -0,0 +1,54 @@
+#dependencies
+# pip3  install Adafruit_Python_DHT
+# pip3 install Flask
+# pip3 install Flask-RESTful
+# pip3 install Flask-Cors
+try:
+	from flask import Flask
+	from flask_restful import Resource,Api
+	from flask_restful import reqparse
+	from flask import request
+	from flask_cors import CORS
+	import time
+	import datetime
+	import json
+	import adafruit_dht
+	import board
+	print("All modules loaded ")
+except Exception as e:
+	print("Error: {}".format(e))
+app = Flask(__name__)
+
+#api_v1_cors_config = {
+#	"origin": ["http://localhost:5173"],
+#	"methods": ["OPTIONS","GET","POST"]
+#}
+
+#CORS(app, resources={
+#	r"/api/v1/": api_v1_cors_config
+#})
+CORS(app)
+
+api = Api(app)
+#pin = 7
+sensor = adafruit_dht.DHT11(board.D4)
+class Sensors(object):
+	def __init__(self):
+		pass
+	def get(self):
+		temperature = sensor.temperature
+		humidity = sensor.humidity
+		if humidity is not None and temperature is not None:
+			return{
+				'Temperature':temperature,
+				"humidity":humidity
+			}
+class Controller(Resource):
+	def __init__(self):
+		pass
+	def get(self):
+		helper = Sensors()
+		return helper.get()
+api.add_resource(Controller, "/api/v1/hello")
+if __name__ == "__main__":
+	app.run(host='0.0.0.0')
